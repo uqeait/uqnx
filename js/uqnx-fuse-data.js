4218763b090aa766733f12e16593db1aa82c5d29
uqeait <uqtscho3@uq.edu.au>
2024-02-08 11:59:56 +1000

    Added more content search tested and successful

--- js/uqnx-fuse-data.js
+++ js/uqnx-fuse-data.js
@@ -3,10 +3,16 @@ window.addEventListener("load", function () {
     {
       id: "whatIsSiemensNX",
       question: "What is Siemens NX?",
-      answer:
-        "NX is an advanced high-end CAD/CAM/CAE. It is used for design, analysis and manufacturing. It is intended as a one stop shop (OSS) solution for design, manufacturing and team work in various engineering fields.",
+      answer: "NX is an advanced high-end CAD/CAM/CAE. It is used for design, analysis and manufacturing. It is intended as a one stop shop (OSS) solution for design, manufacturing and team work in various engineering fields.",
       category: "Home",
       sectionID: "nxHome", 
+    },
+    {
+      id: "howToAccessNX",
+      question: "How To Access NX Software at UQ",
+      answer: "<h3 class=\"text-uq\">EAIT Lab PCs at UQ</h3>\n<p>All EAIT Lab PCs at UQ have Siemens NX installed. This includes 50-N301, 49-316a and 45-203 (among others).</p>\n<h3 class=\"text-uq\">Student Computer [Windows Operating System Only]</h3>\n<ul>\n<li>You can download and install NX to a personal computer/laptop. <a href=\"https://student.eait.uq.edu.au/software/siemens/NX/index.html\" title=\"Install NX on a personal computer at UQ\" target=\"_blank\">Carry out these steps</a> to install Siemens NX on personal computer.</li>\n<li>When not on campus and connected to the UQ network/Wi-Fi (such as at home or on your mobile hotspot) students will need to <a href=\"https://my.uq.edu.au/information-and-services/information-technology/working-remotely/vpn-virtual-private-network\" title=\"Install UQ VPN\" target=\"_blank\">install and use the UQ VPN</a> to allow NX to connect to the license server.</li>\n</ul>\n<h3 class=\"text-uq\">EAIT Remote Desktop</h3>\n<p>Using the Remote Desktop is the recommended option for students when not using EAIT Lab PC’s. <a href=\"https://student.eait.uq.edu.au/infrastructure/remote-access/rdp.html\" target=\"_blank\" title=\"Remote Desktop Download and Install\">Download and install the Remote Desktop</a> and access Siemens NX as you would on an EAIT Lab PC.</p>\n",
+      category: "Home",
+      sectionID: "nxHome",          
     },
   ];
 
@@ -60,7 +66,11 @@ window.addEventListener("load", function () {
               ? `<img src="${content.image}" alt="${content.question}">`
               : ""
           }
-          <a href="#" onclick="switchTabAndScroll('${content.sectionID}', '${content.id}')">Go to content</a>
+          <a class="btn btn-primary d-block w-25 mx-auto" href="#" onclick="switchTabAndScroll('${
+            content.sectionID
+          }', '${
+              content.id
+            }')">Jump to result <i class="fa fa-fast-forward"></i></a>
         `;
           })
           .join("");
@@ -80,6 +90,28 @@ window.addEventListener("load", function () {
     document.getElementById(tabId + "Btn").click();
 
     // Scroll to the content
-    document.getElementById(contentId).scrollIntoView();
+    setTimeout(function(){document.getElementById(contentId).scrollIntoView();}, 500)
   };
+  const searchInput = document.getElementById("uqnxManualsSearch");
+  const clearSearchBtn = document.getElementById("clearSearch");
+
+  searchInput.addEventListener("input", (e) => {
+    // Show the "X" button when the user types in the search input
+    if (e.target.value) {
+      clearSearchBtn.style.display = "block";
+    } else {
+      clearSearchBtn.style.display = "none";
+    }
+  });
+
+  clearSearchBtn.addEventListener("click", () => {
+    // Clear the search input
+    searchInput.value = "";
+    // Clear the results container
+    const resultsContainer = document.getElementById("uqnxManualsSearchResults");
+    resultsContainer.innerHTML = "";
+    // Hide the "X" button
+    clearSearchBtn.style.display = "none";
+    searchInput.focus();
+  });
 });
